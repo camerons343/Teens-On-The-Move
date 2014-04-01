e6ec2a676cd65d8be77863778d44ab1685e5b662
Cameron Smith <camerons343@gmail.com>
2014-04-01 07:13:00 -0700

    School Turn In

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 		end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, #:registerable, :recoverable, 
+  devise :database_authenticatable, :registerable, :recoverable, 
 		 :rememberable, :trackable, :validatable
 		 
 end
