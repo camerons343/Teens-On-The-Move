d824b2f67844605129f88370994527074f8ddf4e
Cameron Smith <camerons343@gmail.com>
2014-04-10 08:56:50 -0700

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :student_number
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :student_nnumber, :email, :password, :password_confirmation) }
   end
 end
