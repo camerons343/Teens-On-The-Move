cf81ab39da02d984cb6af55b5c9a9f74971e1af1
camerons343 <camerons343@gmail.com>
2014-05-09 08:21:52 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,7 @@ class UsersController < ApplicationController
   def logs
     @logs = Log.where(student_number: current_user.student_number)
   end
+  def update
+    authorize! :assign_roles, @user if params[:user][:assign_roles]
+  end
 end
