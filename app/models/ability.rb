9794d0cb0c65ddcd9c401edc2137435a3a50847c
camerons343 <camerons343@gmail.com>
2014-05-09 08:08:53 -0700

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,10 @@ class Ability
     user ||= User.new #Guest user
     if user.role? :admin
       can :manage, :all
+    elsif user.role? :moderator
+      can :update, Logs
+    else
+      can :manage, Logs, :student_number => current_user.student_number
     end
   end
 end
